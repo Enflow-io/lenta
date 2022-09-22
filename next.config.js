97170c61ce8dec5ec39e418085a6d64250b641a6
Enflow.io <konstantin@enflow.io>
2022-09-22 16:40:37 +0300

    port

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  ignoreBuildErrors: true,
+  typescript: {
+    ignoreBuildErrors: true,
+
+  }
 }
 
 module.exports = nextConfig
