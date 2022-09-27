7bbe046349b44ca8880998752e927ba2946fe91c
Enflow.io <konstantin@enflow.io>
2022-09-27 22:03:50 +0300

    pravo

--- components/slide4/DesktopSlide.tsx
+++ components/slide4/DesktopSlide.tsx
@@ -5,11 +5,11 @@ import Card from './Card';
 const DesktopSlide: FC<{ children: any }> = ({children}) => {
     return (
             <div className={classes.Slide}>
-                <Card top={-110} left={0}/>
-                <Card top={40} left={250}/>
-                <Card top={-110} left={500}/>
-                <Card top={40} left={750}/>
-                <Card top={-110} left={1000}/>
+                <Card icon={1} top={-110} left={0}/>
+                <Card icon={2} top={40} left={250}/>
+                <Card icon={3} top={-110} left={500}/>
+                <Card icon={4} top={40} left={750}/>
+                <Card icon={5} top={-110} left={1000}/>
                 {children}
             </div>
     );
---
+++ components/slide4/decl.d.ts
@@ -0,0 +1,3 @@
+declare module "@iconfu/svg-inject" {
+
+}
