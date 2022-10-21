c73925bc0477c02440825118285691e0168a28e1
Enflow.io <konstantin@enflow.io>
2022-10-21 18:54:17 +0300

    talbe

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Layout from "../components/layout/Layout";
-import Search from "../components/search/Search";
+// import Search from "../components/search/Search";
 import React, {useState} from "react";
 import GeoLocation from "../components/GeoLocation/GeoLocation";
 import dynamic from "next/dynamic";
+const Search = dynamic(
+    () => import("../components/search/Search"), // replace '@components/map' with your component's location
+    { ssr: false } // This line is important. It's what prevents server-side render
+);
 
 const Home: NextPage = () => {
     const [loc, setLoc] = useState<string | undefined>(undefined)
     const onLocation = (loc: string)=>{
         setLoc(loc)
     }
-    const Search = dynamic(
-        () => import("../components/search/Search"), // replace '@components/map' with your component's location
-        { ssr: false } // This line is important. It's what prevents server-side render
-    );
+
     return (
         <Layout location={loc}>
 
