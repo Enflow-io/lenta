2e8a7b62819696837ebe857250454bf462f73f1a
Konstantin <hi@enflow.io>
2022-11-24 18:59:16 +0300

    subscription

--- components/Subscribe/Subscribe.tsx
+++ components/Subscribe/Subscribe.tsx
@@ -1,17 +1,18 @@
 import classes from "./Subscribe.module.scss";
 import Select from "react-select";
-import React, {useState} from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
+import * as Lockr from "lockr";
 const options = [
-    {value: '1', label: 'Один раз в день'},
-    {value: '3', label: 'Один раз в неделю'},
+    { value: '1', label: 'Один раз в день' },
+    { value: '3', label: 'Один раз в неделю' },
 ]
 
 const options2 = [
-    {value: '1', label: '2 недели'},
-    {value: '2', label: '1 месяца'},
-    {value: '3', label: '3 месяцев'},
-    {value: '4', label: '6 месяцев'}
+    { value: '1', label: '2 недели' },
+    { value: '2', label: '1 месяца' },
+    { value: '3', label: '3 месяцев' },
+    { value: '4', label: '6 месяцев' }
 ]
 
 interface SubscribeProps {
@@ -47,16 +48,61 @@ const selectorStyles = {
         }
     }
 };
-const Subscribe = (props: SubscribeProps) => {
 
+const validateEmail = (email: string) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+
+const Subscribe = (props: SubscribeProps) => {
     const [isSubscribed, setIsSubscribed] = useState(false)
+    const [email, setEmail] = useState("");
+    const [frequency, setFrequency] = useState(1);
+
+    const subscribe = async () => {
+
+        if(!validateEmail(email)){
+            alert('введите корректный email')
+            return;
+        }
+
+        const savedCity = Lockr.get('city', {id: 99});
+
+        const url = "https://lenta-career-api.axes.pro/api/v1/subscription";
+        const data = {
+
+            "email": email,
+            "subscriptionFrequencyId": frequency,
+            "key": "sddaew",
+            "cityId": savedCity.id,
+            "businessDirectionIdList": [
+                // 1, 3
+            ],
+            "metroIdList": [
+            ]
+        }
+
+        await axios.post(url, data, {
+            headers: {
+                "XApiKey": "ApiKey dw1cv3cXew",
+                "Content-Type": "application/json"
+            }
+        });
+
+
+    }
+
     return <div className={classes.Subscribe}>
         <div className={classes.CloseBtn} onClick={props.close}>
             <svg width="20" height="20" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M20.1733 20.3139L6.73828 6.87891" stroke="white" stroke-width="1.28528"
-                      stroke-linecap="round" stroke-linejoin="round"/>
+                    stroke-linecap="round" stroke-linejoin="round" />
                 <path d="M6.68216 20.1792L20.1172 6.74414" stroke="white" stroke-width="1.28528"
-                      stroke-linecap="round" stroke-linejoin="round"/>
+                    stroke-linecap="round" stroke-linejoin="round" />
             </svg>
         </div>
 
@@ -64,7 +110,9 @@ const Subscribe = (props: SubscribeProps) => {
         {!isSubscribed && <>
             <h3 className={classes.Header}>Параметры рассылки</h3>
             <div className={classes.Divider}></div>
-            <input placeholder={'Введите ваш e-mail'}/>
+            <input onChange={e=>{
+                setEmail(e.target.value)
+            }} placeholder={'Введите ваш e-mail'} />
 
             <div>
                 <label>Отправлять уведомление о подходящих вакансиях: </label>
@@ -76,6 +124,9 @@ const Subscribe = (props: SubscribeProps) => {
                     placeholder={<div className={classes.Placeholder}>Один раз в день</div>}
                     className={classes.ReactSelect}
                     options={options}
+                    onChange={(val)=>{
+                        console.log(val);
+                    }}
                 />
 
             </div>
@@ -90,13 +141,17 @@ const Subscribe = (props: SubscribeProps) => {
                     placeholder={<div className={classes.Placeholder}>2 недели</div>}
                     className={classes.ReactSelect}
                     options={options2}
+                    onChange={(val)=>{
+                        console.log(val);
+                    }}
                 />
 
             </div>
 
             <p>Вы можете подписаться на рассылку новых вакансий, для этого заполните, пожалуйста, поля выше. Подписка
                 будет создана с учетом следующих параметров – Город</p>
-            <button onClick={()=>{
+            <button onClick={async () => {
+                await subscribe();
                 setIsSubscribed(true)
             }
             }>Подписатья на рассылку</button>
